ffe149dbcab0f946edf70cf54e2cd41cf0dd2226
hzn7ry <hzn7ry@DESKTOP-FDUL6OR>
2018-10-21 14:05:07 +0200

    widok_zaj04_01

---
+++ Database/Contoso/dbo/Views/widok_zaj04_01.sql
@@ -0,0 +1,10 @@
+﻿create view [widok_zaj04_01]
+as
+	SELECT	
+		  DATEPART(YEAR,[Datekey]) as [Year],
+		  DATEPART(MONTH,[Datekey]) as [MONTH],
+		  	sum([CostAmount]) as [Koszty]
+
+	FROM [dbo].[FactITMachine]
+	where Costtype='Annual Maintenance Cost'
+	group by Datekey
